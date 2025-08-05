cced280dbf1f126edae79e182b624a507d40cd41
caetanojpo <106156212+caetanojpo@users.noreply.github.com>
2025-08-05 00:51:31 -0300

    feat: image como fallback

--- src/components/backgrounds/VideoBackground.tsx
+++ src/components/backgrounds/VideoBackground.tsx
@@ -1,29 +1,25 @@
-'use client';
-import React, {useState} from 'react';
+import React from 'react';
 
 const VideoBackground = () => {
-    const [hasError, setHasError] = useState(false);
 
     return (
         <div className="absolute top-0 left-0 w-full h-full z-0 overflow-hidden">
-            {hasError ? (
+            <video
+                className="w-full h-full object-cover"
+                src="https://ltc97ny8hknut0of.public.blob.vercel-storage.com/videoBg.mp4"
+                autoPlay
+                muted
+                loop
+                playsInline
+                preload="auto"
+            >
                 <img
                     src="/home-banner-bg.png"
                     alt="Video fallback"
                     className="w-full h-full object-cover"
                 />
-            ) : (
-                <video
-                    className="w-full h-full object-cover"
-                    src="https://ltc97ny8hknut0of.public.blob.vercel-storage.com/videoBg.mp4"
-                    autoPlay
-                    muted
-                    loop
-                    playsInline
-                    onError={() => setHasError(true)}
-                >
-                </video>
-            )}
+                Your browser does not support the video tag.
+            </video>
         </div>
     );
 };
