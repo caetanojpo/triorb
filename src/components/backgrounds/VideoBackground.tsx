e32822a9c057088a4099cc937b0e7af821946804
caetanojpo <106156212+caetanojpo@users.noreply.github.com>
2025-08-08 11:38:20 -0300

    feat(triorb): trigger video

--- src/components/backgrounds/VideoBackground.tsx
+++ src/components/backgrounds/VideoBackground.tsx
@@ -5,7 +5,7 @@ const VideoBackground = () => {
     <div className="absolute top-0 left-0 w-full h-full z-0 overflow-hidden">
       <video
         className="w-full h-full object-cover"
-        src="https://ltc97ny8hknut0of.public.blob.vercel-storage.com/videoBg.mp4"
+        src="/videoBg.mp4"
         autoPlay
         muted
         loop
