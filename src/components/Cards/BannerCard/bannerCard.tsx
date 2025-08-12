868a5954ce70da3394fb1a2da67dc02558b15cdb
caetanojpo <106156212+caetanojpo@users.noreply.github.com>
2025-08-11 23:15:10 -0300

    feat(triorb): remove banner card

--- src/components/Cards/BannerCard/bannerCard.tsx
+++ src/components/Cards/BannerCard/bannerCard.tsx
@@ -6,25 +6,21 @@ import { motion } from 'framer-motion';
 const BannerCard = () => {
   return (
     <motion.div
-      className={'flex h-full w-full flex-col justify-center items-center rounded-xl'}
+      className={'flex h-[70%] w-full flex-col justify-center items-center rounded-xl'}
       whileHover={{
         scale: 1.01,
         boxShadow: '0 25px 50px rgba(59, 130, 246, 0.2)',
       }}
     >
-      <div className={'hidden 2xl:block h-[80%] w-full relative'}>
-        <Image src={'/tecnologia.png'} alt={''} fill className={'rounded-t-xl'} />
-      </div>
-      <div
-        className={
-          'flex flex-col  w-full h-full bg-white text-text p-4 rounded-t-xl 2xl:rounded-t-[0px] rounded-b-xl'
-        }
-      >
+      {/*<div className={'hidden 2xl:block h-[80%] w-full relative'}>*/}
+      {/*  <Image src={'/tecnologia.png'} alt={''} fill className={'rounded-t-xl'} />*/}
+      {/*</div>*/}
+      <div className={'flex flex-col  w-full h-full bg-white text-text p-4 rounded'}>
         <div className={'flex w-full flex-col justify-center items-center gap-2 font-bold p-6'}>
-          <h5 className={'text-[1.5rem] 2xl:text-[1.7rem] leading-8 2xl:leading-0'}>
+          <h5 className={'text-[1.3rem] 2xl:text-[1.4rem] leading-8 2xl:leading-4'}>
             TECNOLOGIA 3D PARA TRANSFORMAR
           </h5>
-          <h4 className={'text-[2.5rem] 2xl:text-[2.8rem] text-secondary-500'}>
+          <h4 className={'text-[2rem] 2xl:text-[2.3rem] text-secondary-500'}>
             O MERCADO IMOBILIÁRIO
           </h4>
           <div className={'w-full h-[1px] bg-gray-300'} />
